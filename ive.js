06b1bb96fd21a381d0303cd14e9f2cacbd1b8d50
陈玲钰 <chenlingyu@innotechx.com>
2021-08-10 15:50:11 +0800

    feat: version 1.0

---
+++ ive.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const which = require('which');
+const child_process = require('child_process');
+
+const binary = which.sync('code', { nothrow: true });
+if (!binary) {
+  console.log('VS Code not found');
+  return;
+}
+
+const installExt = '--install-extension';
+const recommendations = ['esbenp.prettier-vscode', 'eamodio.gitlens'];
+const args = recommendations.reduce((args, rec) => {
+  args.push(installExt);
+  args.push(rec);
+  return args;
+}, []);
+
+const vscode = child_process.spawn(binary, args);
+
+vscode.stdout.on('data', data => process.stdout.write(data));
+vscode.stderr.on('data', data => process.stderr.write(data));
